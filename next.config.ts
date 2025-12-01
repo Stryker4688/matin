6834937e92ccef1e7797d873f5484e9045cd0e62
shayan <shayan.shadman4851321@gmail.com>
2025-12-02 00:28:51 +0330

    s

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // برای خروجی استاتیک
+  basePath: process.env.NODE_ENV === "production" ? "/REPO_NAME" : "", // نام ریپو
+  images: {
+    unoptimized: true, // برای GitHub Pages نیاز است
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  trailingSlash: true, // برای GitHub Pages بهتر است
 };
 
 export default nextConfig;
